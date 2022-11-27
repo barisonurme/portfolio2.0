0bb5e88e7fb98feeb943ce0ed40356e555d3a8b0
barisonurme <barisonurme@icloud.com>
2022-11-27 12:27:32 +0300

    Hamburger Menu Ui Update.

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -13,10 +13,28 @@ const HamburgerMenu = (props) => {
           className="flex backdrop-blur-xl flex-col bg-slate-800/10 inset-0 justify-center items-center w-full h-screen fixed z-40 text-white"
         >
           <div className="grid gap-8 text-5xl  text-center">
-            <div className="hover:text-rose-500 cursor-pointer">About</div>
-            <div className="hover:text-rose-500 cursor-pointer">Works</div>
-            <div className="hover:text-rose-500 cursor-pointer">Skills</div>
-            <div className="hover:text-rose-500 cursor-pointer">Contact</div>
+            <div className="group hover:text-rose-500 duration-500 cursor-pointer flex flex-col justify-center items-center">
+              About
+              <div className="h-2 group-hover:w-32 opacity-0 group-hover:opacity-100 bg-rose-500 w-0 flex duration-500"></div>
+            </div>
+            <div className="grid gap-8 text-5xl  text-center">
+              <div className="group hover:text-rose-500 duration-500 cursor-pointer flex flex-col justify-center items-center">
+                Projects
+                <div className="h-2 group-hover:w-44 opacity-0 group-hover:opacity-100 bg-rose-500 w-0 flex duration-500"></div>
+              </div>
+            </div>
+            <div className="grid gap-8 text-5xl  text-center">
+              <div className="group hover:text-rose-500 duration-500 cursor-pointer flex flex-col justify-center items-center">
+                Skills
+                <div className="h-2 group-hover:w-32 opacity-0 group-hover:opacity-100 bg-rose-500 w-0 flex duration-500"></div>
+              </div>
+            </div>
+            <div className="grid gap-8 text-5xl  text-center">
+              <div className="group hover:text-rose-500 duration-500 cursor-pointer flex flex-col justify-center items-center">
+                Contact
+                <div className="h-2 group-hover:w-40 opacity-0 group-hover:opacity-100 bg-rose-500 w-0 flex duration-500"></div>
+              </div>
+            </div>
           </div>
         </div>
       )}

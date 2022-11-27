f5f5ea97a3fb8d0004fa942b2c0433616df8e77b
barisonurme <barisonurme@icloud.com>
2022-11-27 14:21:29 +0300

    class changed to className

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -3,15 +3,15 @@ import React from "react";
 const Tooltip = (props) => {
   const { text } = props;
   return (
-    <div class="-translate-y-2 opacity-0 w-28 bg-black text-white text-center text-xs rounded-lg py-2 absolute z-10 group-hover:opacity-100 bottom-full px-3 pointer-events-none">
+    <div className="-translate-y-2 opacity-0 w-28 bg-black text-white text-center text-xs rounded-lg py-2 absolute z-10 group-hover:opacity-100 bottom-full px-3 pointer-events-none">
       {text}
       <svg
-        class="absolute text-black h-2 w-full left-0 top-full"
+        className="absolute text-black h-2 w-full left-0 top-full"
         x="0px"
         y="0px"
         viewBox="0 0 255 255"
       >
-        <polygon class="fill-current" points="0,0 127.5,127.5 255,0" />
+        <polygon className="fill-current" points="0,0 127.5,127.5 255,0" />
       </svg>
     </div>
   );
